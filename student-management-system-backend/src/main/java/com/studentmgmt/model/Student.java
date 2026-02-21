9fe8bd4344bd266b87c5bebd8250c6b0881bcb2e
SHREYAS WANKAR <23070009@ycce.in>
2026-02-21 10:37:01 +0530

    Add Student class for student management system

---
+++ student-management-system-backend/src/main/java/com/studentmgmt/model/Student.java
@@ -0,0 +1,36 @@
+package com.studentmgmt.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String firstName;
+    
+    @Column(nullable = false)
+    private String lastName;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String phoneNumber;
+    
+    @Column(nullable = false)
+    private String address;
+    
+    @Column(nullable = false)
+    private String course;
+}
